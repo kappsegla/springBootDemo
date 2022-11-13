5ea667ac03518dd4487767911a7e9ccae243bd13
Martin Blomberg <martin.blomberg@outlook.com>
2022-11-13 22:01:21 +0100

    Cleaned Project

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,55 +5,51 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class DemoController {
-    
+
     PersonRepository personRepository;
 
-    public DemoController(PersonRepository personRepository){
+    public DemoController(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-
-    @GetMapping("/demo")
-    public String demo(){
-        return "Hello there";
-    }
-
     @GetMapping("/persons")
-    public List<Person> getPerson(){
-        return List.of(new Person("Martin", 45, "Kalmar"),
-        new Person("Kalle", 30, "Lund"));
+    public List<Person> getPerson() {
+        return List.of();
     }
 
     @GetMapping("/persons/{id}")
-    public Person getOnePerson(@PathVariable int id){
-        return new Person("Kalle", 45, "Kalmar");
+    public Person getOnePerson(@PathVariable int id) {
+        return null;
     }
 
+    // @PostMapping("/persons")
+    // @ResponseStatus(HttpStatus.CREATED)
+    // public Person addPerson(@RequestBody Person person) {
+    //     return personRepository.save(person);
+    // }
+    
     @PostMapping("/persons")
-    public String addPerson(@RequestBody Person person){
-        //Validate person
-        personRepository.save(person);
-        return "Person saved";
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+            
+        var p = personRepository.save(person);
+        return  ResponseEntity.created(
+            ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(p.getId())
+            .toUri())
+            .build();
     }
-
-  /*   @GetMapping("/sorting")
-    public String demoSorting(@RequestParam String order){
-        return order;
-    }
-*/
-    @GetMapping("/sorting")
-    public Set<Entry<String, String>> demoSorting(@RequestParam Map<String,String> urlParams){
-        return urlParams.entrySet();
-    }
-
 
 }
