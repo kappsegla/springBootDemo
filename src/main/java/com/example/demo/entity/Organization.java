a6398bcac5b2b19bb72bdae3fea7a3fc44bf6f8c
Martin Blomberg <martin.blomberg@outlook.com>
2022-11-17 11:59:50 +0100

    Lazy loading default values

--- src/main/java/com/example/demo/entity/Organization.java
+++ src/main/java/com/example/demo/entity/Organization.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,8 +32,12 @@ public class Organization {
     @UpdateTimestamp
     private LocalDateTime modified;
 
+// OneToMany: LAZY
+// ManyToOne: EAGER
+// ManyToMany: LAZY
+// OneToOne: EAGER
 
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch=FetchType.EAGER)
     private Set<Member> members = new HashSet<>();
 
     public Long getId() {
