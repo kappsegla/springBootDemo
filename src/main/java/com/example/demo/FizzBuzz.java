9a061f3559c6aff6c0d30fe2d0bdaf06c7fedcdc
Martin Blomberg <martin.blomberg@outlook.com>
2022-11-29 10:31:44 +0100

    Add parameterized test

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     public static String fizzBuzz(String string) {
         if( string.equals("3"))
             return "Fizz";
+        if( string.equals("5"))
+            return "Buzz";            
         return string;
     }
     
--- src/test/java/com/example/demo/FizzBuzzTests.java
+++ src/test/java/com/example/demo/FizzBuzzTests.java
@@ -1,35 +1,46 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class FizzBuzzTests {
 
-
     @Test
-    void givenValueZeroShouldReturnZero(){
-        
+    void givenValueZeroShouldReturnZero() {
+
         String result = FizzBuzz.fizzBuzz("0");
 
         assertThat(result).isEqualTo("0");
     }
 
     @Test
-    void givenValueOneShouldReturnOne(){
-        
+    void givenValueOneShouldReturnOne() {
+
         String result = FizzBuzz.fizzBuzz("1");
 
         assertThat(result).isEqualTo("1");
     }
 
     @Test
-    void givenValueThreeShouldReturnFizz(){
-        
+    void givenValueThreeShouldReturnFizz() {
+
         String result = FizzBuzz.fizzBuzz("3");
 
         assertThat(result).isEqualTo("Fizz");
     }
 
+    @ParameterizedTest(name = "For value {0} should return {1}")
+    @CsvSource({
+            "4,4",
+            "5,Buzz"
+    })
+    void shouldReturnFizzBuzz(String value, String expectedResult) {
+        String result = FizzBuzz.fizzBuzz(value);
 
+        assertThat(result).isEqualTo(expectedResult);
+    }
 
 }
