238ba6fef62126e7c4fa9e51573aa1adaadf4d9a
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-05 11:30:54 +0100

    Add more tests Fixes #2 with a new test for /orgs endpoint.

--- src/test/java/com/example/demo/controller/OrganizationControllerMVCTest.java
+++ src/test/java/com/example/demo/controller/OrganizationControllerMVCTest.java
@@ -1,24 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.example.demo.entity.Organization;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.OrganizationRepository;
 import com.example.demo.service.AgeService;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OrganizationController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -36,7 +40,23 @@ public class OrganizationControllerMVCTest {
 
     @Test
     void callingEndpointGetOrgsShouldReturn200OK() throws Exception {
-        mockMvc.perform( MockMvcRequestBuilders.get("/orgs"))
-        .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform( get("/orgs"))
+        .andExpect(status().isOk());
+    }
+
+    @Test
+    void getOrganizationsShouldReturnListOfOrganizations() throws Exception{
+        var org = new Organization();
+        org.setName("Testorg");
+        org.setId(1L);
+        Mockito.when(orgrepo.findAll()).thenReturn(List.of(org));
+
+        mockMvc.perform( get("/orgs"))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$[0].name").value("Testorg"))
+        .andExpect(jsonPath("$[0].id").value("2"));
+        
+
+
     }
 }
